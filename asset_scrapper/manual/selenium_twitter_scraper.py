b65ac97b1baf709139761b52658eaedab166b55b
Marco Cen <marco.c3n@gmail.com>
2023-01-03 20:21:30 -0500

    Cleanup Code, Rewritten logic for better ease of modification and readability/traceability of code, docstrings
    Its ALIVE. New stockup lore dropped

--- asset_scrapper/manual/selenium_twitter_scraper.py
+++ asset_scrapper/manual/selenium_twitter_scraper.py
@@ -1,19 +1,34 @@
-# Twitter Scraper Python using Selenium
-#NOTE:
-# - Uses dummy Twitter account
-# - Runs UNTIL load request reached? (Due to infinite scrolling)
-# - Fullscreen for best results (Certain elements only appear with certain resolutions)
-# - ~10-15 min runtime
-#TODO: 
-# - When collect data, remove new line in TWEET TEXT for better data handling during collection?
-# - More efficient runtime?
-# - Dynamic 'Search Query', 'Latest' tab?
-# - Dynamically setup SEARCH TERM?
-# - DOESNT support 'tweetTextAddon' (Person referencing tweet below. How to store that data in database so useful?)
-# - Setup ENV variables? (Replace username, pswd, etc... from file)
-# - Put time limit on how long extract data?
+"""
+Twitter Cards Scraper Python using Selenium
+NOTE:
+ Overview Objective WorkFlow: 
+ 1. (Navigation) Log into Twitter --> Searches inputted keyword query --> Select "LATEST" tab
+ 2. (Collection) EXTRACTS & COLLECTS Tweet Cards seen on page --> Scrolls --> REPEAT until condition met to stop... --> STORE all Tweet Cards extracted & collected
+ (Setup)
+ - Forces fullscreen for best/foolproof results (Certain elements only appear with certain resolutions)
+ - Uses dummy Twitter account
+ (Collection)
+ - Filters DURING collection of tweet cards: (view docstrings of EXTRACT and COLLECT functions)
+ - Condition used to stop infinite scrolling: (If current scroll position >= total scrollable height of page) OR (scroll count) 
+ - Runtime WITHOUT set scroll Count limit/condition: ~10-15 min runtime (Request overload maybe?)
+ (Storing)
+ - 
+TODO: 
+- Setup ENV variables in file for login credentials + other miscellaneous items? (Good code practice)
+(Navigation)
+ - With user input, dynamically setup custom navigation on Twitter?: 
+    - Enter keyword for search query box (ie: '#TSLA')
+    - Switch case to see various tab options to select (ie: 'Latest' tab)
+(Collection)
+ - DOESNT support certain elements:
+    - 'tweetTextAddon' = Person referencing tweet below (Needed or useful to collect and store?)
+    - Images/gifs/videos/media text (Needed or useful to collect and store?)
+ - Filter further DURING collection of tweet cards? (ie: Only english?)
+ - Filter further AFTER storing the extracted & collected data? (Ie: Better data handling if continue to create own file, otherwise store in DATABASE)
+ - More efficient runtime? (Better handling of infinite scrolling!)
+"""
 
-import time, csv
+import time, csv, os
 from time import sleep
 
 from selenium.webdriver import Chrome #Firefox Browser: "Firefox" | Edge Browser: "from msedge.selenium_tools import Edge, EdgeOptions"
@@ -25,10 +40,58 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 
 
-startTime = time.time() #Time Keeper
-#-- Function to Automate Collecting Tweet Cards List and their information --
-def get_tweet_data_payload(tweet):
-    """ Extract Data from each Tweet Card Payload"""
+####################### FUNCTIONS ##########################
+def scroll_to_bottom(scrollCount):
+    """ Function handles infinite scrolling: scrolls all the way to bottom of page: using Selenium
+        - Includes EXTRACT and COLLECT of tweet cards shown on page
+        Returns: TRUE if (bottom of page/condition) REACHED (No more tweets are loaded)
+    """
+    #-- SCRAPE and COLLECT all the tweets loaded on entire page using Selenium
+    collect_tweet_data_payload()
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") # Scroll to bottom of page
+    time.sleep(2) # Wait for the page to load
+
+    print("-- Scroll Count: " + str(scrollCount) + " --")
+
+    #TODO: EXIT FUNCTION (2 Ways with: scroll pos >= total height of page   OR   scroll counter)
+    # # Get NEW scroll height
+    # newScrollHeightPos = driver.execute_script("return Math.max( \
+    #     document.body.scrollHeight, document.body.offsetHeight, \
+    #     document.documentElement.clientHeight, \
+    #     document.documentElement.scrollHeight, \
+    #     document.documentElement.offsetHeight );")
+
+    # #-- Have we reached bottom of the page? Is there no more new tweets to load?
+    # if newScrollHeightPos != currentScrollHeightPos: # Update the last scroll height to set the new CURRENT Y scroll height pos
+    #     currentScrollHeightPos = newScrollHeightPos  
+    #     return False 
+    # elif newScrollHeightPos == currentScrollHeightPos: # Bottom of the page reached if CURRENT Y scroll height pos MATCHES with NEW Y scroll height pos (No more scrolling/change in Y scroll height pos)
+    #     return True
+    # return newScrollHeightPos == currentScrollHeightPos #JIC failsafe
+
+#######
+    # if newScrollHeightPos == currentScrollHeightPos: # If the new scroll height is the same as the last scroll height, it means we have reached the bottom of the page
+    #     break
+
+    # # Update the last scroll height
+    # currentScrollHeightPos = newScrollHeightPos
+##########
+
+    # return driver.execute_script("return {currentScrollHeightPos} >= document.body.scrollHeight;") # (Compare If current scroll position is MORE/EQUAL than the total scrollable height of the page) [DOESNT WORK??]
+    #-- [OR] --
+    #-- Change # of scrolls to set Limit/Condition to stop
+    return scrollCount == 10
+
+
+def extract_tweet_data_payload(tweet):
+    """ Function EXTRACTS data details from EACH Tweet Card: using Selenium 
+        - Extracts and Stores: username, twitterHandle, datePosted, replyCount, retweetCount, LikeCount, tweetText
+        - Data filtering: 
+            - OMIT ads/sponsors/non-users by checking for no timestamps        
+        Returns: Tuple Object of specific tweet card 
+    """
+
     username = tweet.find_element("xpath", './/span').text # Get Username  (Elements identified earlier are removed from DOM due to page refresh)
     twitterHandle = tweet.find_element("xpath", './/span[contains(text(), "@")]').text # Get Twitter Handle
     try:
@@ -49,9 +112,51 @@ def get_tweet_data_payload(tweet):
 
     # Combine into Tuple: Tweet data
     tweetData = (username, twitterHandle, datePosted, replyCount, retweetCount, LikeCount, tweetText)
+    
     return tweetData
 
 
+def collect_tweet_data_payload():
+    """ Function COLLECTS list of Tweets payload extracted: using Selenium 
+        Returns: Finalized list of Tuple Objects which are all the tweet cards extracted and collected
+    """
+    tweetCardList = driver.find_elements("xpath", '//article[@data-testid="tweet"]') # Get entire Tweet Card Payload element
+    for tweet in tweetCardList: # [-15:]:  # View LAST 15 items (Removes need to recheck every tweet in list & reduces duplicates)
+        data = extract_tweet_data_payload(tweet)
+        if data:
+            #-- Check for duplicates collected (Make our OWN tweet ID)
+            tweetId = ''.join(data) # Create UNIQUE ID by concat elements of tweet into 1 string
+            if tweetId not in tweetIdList:
+                tweetIdList.add(tweetId)
+                payloadCollected.append(data) # Original Scrapped Data
+    
+    print("-- Extracted and Collected Tweets Payload --")
+    return payloadCollected
+
+
+def store_tweet_data_payload(dataPayload):
+    """ Function STORES list of Tweets payload extracted and collected in desired format: using Selenium """
+
+    # TO CSV
+    directoryPath = 'CrawlScrap/asset_scrapper/api/database/data_storage'
+    createdfileName = 'TwitterPayloadScrapped.csv'
+    csvFile = os.path.join(directoryPath, createdfileName)
+
+    with open(csvFile, 'w', newline='', encoding='utf-8') as file: # Writing to file
+        header = ['Username', 'Twitter Handle', 'Timestamp',
+                  "Comments", "Retweets", "Likes", 'Text']
+        writer = csv.writer(file)
+        writer.writerow(header)
+        writer.writerows(dataPayload)
+
+    print("<< STORED Tweets Payload >>")
+
+
+# --------------------------------------------------------------- (DIVIDER) ----------------------------------------------------------------
+    
+
+####################### LOGIC ##########################
+startTime = time.time() #Time Keeper
 
 #-- Create Instance of Webdriver 
 options = ChromeOptions()
@@ -85,7 +190,7 @@ sleep(1) # To allow enough load time
 pswd = driver.find_element("xpath", '//input[@type="password"]')
 pswd.send_keys("Testing1") # Pswd for this acc TODO!
 pswd.send_keys(Keys.RETURN)
-# sleep(1) # To allow enough load time
+sleep(1) # To allow enough load time
 
 #-- Go to search box and type keyword to search --
 searchInput = driver.find_element("xpath", '//input[@data-testid="SearchBox_Search_Input"]')
@@ -95,57 +200,58 @@ searchInput.send_keys(Keys.RETURN)
 #-- Pull historical data (Ex: "LATEST" tab) TODO!
 driver.find_element("xpath", '//div[@class="css-901oao r-1awozwy r-14j79pv r-6koalj r-18u37iz r-37j5jr r-a023e6 r-majxgm r-1pi2tsx r-1777fci r-rjixqe r-bcqeeo r-1l7z4oj r-95jzfe r-bnwqim r-qvutc0"]').click() #Just above span tag bc cant interact with span tag
 
-#-- Apply Function to ALL Cards if any data returns --
-tweetDataPayloadList = []
+
+
+
+
+#-- Scroll to get entire PAGE of Tweets (Either bottom of the page or set limit condition reached) --
+scrollCount = 0
+payloadCollected = []
 tweetIdList = set()
-lastPosOfScroll = driver.execute_script("return window.pageYOffset;") # Keep track of y position of scroll bar (Know when we reached end of pagination)
-scrolling = True # Fixes pagination bug where diff y Offset of scroll pos not correctly recognized (Due to load times)
+# Get CURRENT scroll height
+currentScrollHeightPos = driver.execute_script("return Math.max( \
+    document.body.scrollHeight, document.body.offsetHeight, \
+    document.documentElement.clientHeight, \
+    document.documentElement.scrollHeight, \
+    document.documentElement.offsetHeight );")
 
-#-- Scrolling to Get New Tweet Data Loaded --
-while scrolling: 
+while True:
+    try:
+        # Reached End of Page or Limit Condition Reached
+        scrollCount += 1
+        if scroll_to_bottom(scrollCount): # Reminder: in Python function is called here as well in condition statement!
+            print("-- [All Tweets on Page LOADED in] --")            
+            break # EXIT while loop 
+    except:
+        continue
 
-##################
-    print("-- Collecting Tweet Data --")
-    tweetCardList = driver.find_elements("xpath", '//article[@data-testid="tweet"]') # Get entire Tweet Card Payload element
-    for tweet in tweetCardList [-15:]:  # View LAST 15 items (Removes need to recheck every tweet in list & reduces duplicates)
-        data = get_tweet_data_payload(tweet)
-        if data:
-            #-- Check for duplicates collected (Make our OWN tweet ID)
-            tweetId = ''.join(data) # Create UNIQUE ID by concat elements of tweet into 1 string
-            if tweetId not in tweetIdList:
-                tweetIdList.add(tweetId)
-                tweetDataPayloadList.append(data) # Original Scrapped Data
-##################
+# [Uncomment below ONLY if want to scrap stuff AFTER reach bottom of page, other than scrap as it scrolls]
+# #-- SCRAPE and COLLECT all the tweets loaded on entire page using Selenium 
+# payloadCollected = collect_tweet_data_payload()
 
-    scrollAttempt = 0
-    while True:
-        # -- Continuously scroll down page (Pagination on Twitter: unlimited scrolling)
-        print("<< Scrolling Down the Page for More Tweets... >>")
-        driver.execute_script('window.scrollTo(0, document.body.scrollHeight);') 
-        sleep(2) # To allow enough load time
+#-- STORE Data Collected
+store_tweet_data_payload(payloadCollected)
 
-        #-- Keep track of y pos of scroll bar: Handle pagination and different contents
-        currPosOfScroll = driver.execute_script("return window.pageYOffset;")
-        if currPosOfScroll == lastPosOfScroll:  # If same, break OUT of loop 
-            scrollAttempt += 1
+print("--- %s Minutes ---" % ((time.time() - startTime) / 60))  # Reference for time keeping sake 
 
-            #-- END scroll region
-            if scrollAttempt >= 3:
-                scrolling = False
-                break
-            else:
-                sleep(2) # Attempt to scoll again
-        else:
-            lastPosOfScroll = currPosOfScroll 
-            break
+#-- CLOSE browser to save resources (Good practice)
+driver.close() 
+# driver.quit() # Close Multiple Windows 
 
 
-#-- Saving Twitter Payload data collected --
-# (TO CSV)
-with open('pythonSeleniumTweetPayloadData.csv', 'w', newline='', encoding='utf-8') as f:
-    header = ['Username', 'Twitter Handle', 'Timestamp', "Comments", "Retweets", "Likes", 'Text']
-    writer = csv.writer(f)
-    writer.writerow(header)
-    writer.writerows(tweetDataPayloadList)
-    print("<< COLLECTED TWEET DATA NOTED. >>")
-    print("--- %s Minutes ---" % ((time.time() - startTime) / 60))
+
+
+
+
+
+
+
+
+
+
+# Code Commit Msg
+"""
+Subject: Its ALIVE. NEW stockup LORE dropped 
+- cleanup code/file structure
+- alternative cleaner method: Ease of modification and readability/traceability of code
+"""
