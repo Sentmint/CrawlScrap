5ff6d4ede5b821ccd18076746c7bdf54609d9dc6
idzidz <idz2612@gmail.com>
2023-01-29 05:33:24 -0500

    fetches all comments from front page posts sorted by hot; duplicate comment handling needs to be implemented

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import praw
 from praw.models import MoreComments
 import secrets
+import datetime
 
 reddit = praw.Reddit(
     client_id=secrets.creds.get('CLIENT_ID'),
@@ -8,14 +9,37 @@ reddit = praw.Reddit(
     user_agent=secrets.creds.get('USER_AGENT')
 )
 
-hot_posts = reddit.subreddit('WallStreetBets').hot(limit=20)
-# for x in hot_posts:
-#     print(x.title)
-#     print(x)
+## TODO: find a way to get to the bottom of every comment, or find a function that does it for us.
+## TODO: think of a way to exclude duplicates in the final product. Ignore already stored comments, only rescan when size has changed.
 
-submissionTest = reddit.submission(id="10nnswb")
 
-for x in submissionTest.comments:
-    if isinstance(x, MoreComments or x.parent_id != "10nnswb"):
+# def threadScrape(thread):
+
+# Each page is 25 entries on reddit. Only checking the front page of the subreddit.
+note01 = reddit.subreddit("wallstreetbets").hot(limit=25)
+note00 = reddit.submission("10nozyf")
+
+# for x in note01:
+#     if x.stickied:
+#         continue
+#     print(x.name, ", ", x.author, ": ", x.title, x.selftext)
+
+testList = []
+
+print(len(testList))
+
+start = datetime.datetime.now()
+
+for x in note01:
+    if x.stickied:
         continue
-    print(x.author, ":", x.body, x.score)
+    curr = reddit.submission(x.id)  # Grabs the front page ids to be scanned for comments
+    curr.comments.replace_more(limit=None)
+    for y in curr.comments.list():
+        testList.append(y.body)
+
+end = datetime.datetime.now()
+
+totalRun = end - start
+
+print(len(testList))
