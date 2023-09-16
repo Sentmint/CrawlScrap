fd63ca5b94d9b1b698dc0f9335d704a7678696dd
Fatimah Vakily <fvaki076@uottawa.ca>
2023-09-15 20:29:42 -0400

    STM-129 made host an os env var

--- reddit/producer.py
+++ reddit/producer.py
@@ -1,16 +1,16 @@
 import pika
 import json
+import os
 
 def publish_stock(stocks_found):
     # Sends the found stocks to the queue  
     connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
+    pika.ConnectionParameters(host=os.environ.get('REDDIT_RABBITMQ_HOST')))
     channel = connection.channel()
 
     channel.queue_declare(queue='scraped_data')
 
     channel.basic_publish(exchange='', routing_key='scraped_data', body=json.dumps(stocks_found))
-    #print(stocks_found)
-    print(" [x] Sent Stock Data to Queue")
+    print("[x] Sent Stock Data to Queue")
     connection.close()
     return
