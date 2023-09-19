2d5428b451646fb8229b06828d9cc41d54a9cce4
Marco-Cen <marco.c3n@gmail.com>
2023-09-19 18:09:21 -0400

    [STM-177]: Successfully  Dockerized Reddit Scraper

--- reddit/reddit_service/producer.py
+++ reddit/reddit_service/producer.py
@@ -3,7 +3,7 @@ import json
 import os
 import logging
 
-def publish_stock(stocks_found, queue_exchange, queue_key):
+def publish_stock(stocks_found, queue_exchange, queue_key, logging):
     # Sends the found stocks to the queue  
     connection = pika.BlockingConnection(
     pika.ConnectionParameters(host=os.environ.get('REDDIT_RABBITMQ_HOST')))
@@ -11,7 +11,8 @@ def publish_stock(stocks_found, queue_exchange, queue_key):
 
     channel.queue_declare(queue=queue_key) # TODO: Make this a passive queue once queue creation is handled
 
-    channel.basic_publish(exchange=queue_exchange, routing_key=queue_key, body=json.dumps(stocks_found))
+    channel.basic_publish(exchange=queue_exchange, routing_key=queue_key, properties=pika.BasicProperties(content_type='application/json'), body=json.dumps(stocks_found))
+    
     logging.info(" [x] Sent Stock Data to Queue")
     connection.close()
     return
